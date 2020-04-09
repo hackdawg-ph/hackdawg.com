03d19a3815a8df136d579bce0be42b0098feb6eb
Jovert Palonpon <jovertical@gmail.com>
2020-04-09 13:06:53 +0800

    Add upload assets workflow

--- deploy/chore.sh
+++ deploy/chore.sh
@@ -2,8 +2,8 @@
 
 # Generate application key
 echo "Generating application key..."
-php artisan key:generate
+php artisan key:generate --force
 
 # Run the migrations
 echo "Running the migrations..."
-php artisan migrate --force
+# php artisan migrate --force
