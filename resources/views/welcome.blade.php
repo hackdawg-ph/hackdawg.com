06a248406592a59d44956c62ed3078bc5cba917f
Jovert Palonpon <jovertical@gmail.com>
2020-04-09 13:10:09 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
-    <p class="text-center font-hairline">Hello, We are Hackdawg...</p>
-    <p class="text-center font-thin">Hello, We are Hackdawg...</p>
-    <p class="text-center font-light">Hello, We are Hackdawg...</p>
-    <p class="text-center font-normal">Hello, We are Hackdawg...</p>
-    <p class="text-center font-medium">Hello, We are Hackdawg...</p>
-    <p class="text-center font-semibold">Hello, We are Hackdawg...</p>
-    <p class="text-center font-bold">Hello, We are Hackdawg...</p>
-    <p class="text-center font-extrabold">Hello, We are Hackdawg...</p>
-    <p class="text-center font-black">Hello, We are Hackdawg...</p>
+    <div class="flex h-screen items-center justify-center">
+        <p class="text-center font-hairline">Hello, We are Hackdawg...</p>
+        <p class="text-center font-thin">Hello, We are Hackdawg...</p>
+        <p class="text-center font-light">Hello, We are Hackdawg...</p>
+        <p class="text-center font-normal">Hello, We are Hackdawg...</p>
+        <p class="text-center font-medium">Hello, We are Hackdawg...</p>
+        <p class="text-center font-semibold">Hello, We are Hackdawg...</p>
+        <p class="text-center font-bold">Hello, We are Hackdawg...</p>
+        <p class="text-center font-extrabold">Hello, We are Hackdawg...</p>
+        <p class="text-center font-black">Hello, We are Hackdawg...</p>
+    </div>
 </x-layout>
