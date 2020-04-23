0ac635d0dfcb55fed2903c915fcb3518320164cf
Jovert Palonpon <jovertical@gmail.com>
2020-04-24 07:12:06 +0800

    fixed: Avatar component rendering broken images

--- resources/console/js/Shared/Avatar.jsx
+++ resources/console/js/Shared/Avatar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import UserCustomIcon from '@console/Shared/Icons/UserCustom';
 
 export default function Avatar({ variant = 'rounded', size = 'md', url = null, className }) {
+    const [hasError, setHasError] = useState(false);
+
     return (
         <span
             className={cx(
@@ -12,14 +14,15 @@ export default function Avatar({ variant = 'rounded', size = 'md', url = null, c
                     'rounded-full': variant === 'rounded',
                     'w-8 h-8': size === 'sm',
                     'w-12 h-12': size === 'md',
+                    'w-16 h-16': size === 'lg',
                 },
                 className,
             )}
         >
-            {url === null ? (
+            {url === null || hasError ? (
                 <UserCustomIcon className="w-full h-full text-gray-300" />
             ) : (
-                <img className="w-full h-full" src={url} alt="Avatar" />
+                <img className="w-full h-full" src={url} onError={() => setHasError(true)} alt="Avatar" />
             )}
         </span>
     );
@@ -27,7 +30,7 @@ export default function Avatar({ variant = 'rounded', size = 'md', url = null, c
 
 Avatar.propTypes = {
     variant: PropTypes.oneOf(['rounded']),
-    size: PropTypes.oneOf(['sm', 'md']),
+    size: PropTypes.oneOf(['sm', 'md', 'lg']),
     url: PropTypes.string,
     className: PropTypes.string,
 };
