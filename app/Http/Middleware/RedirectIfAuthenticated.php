3397aeca909670cddf473f779b429aa34f2d7f11
Jovert Palonpon <jovertical@gmail.com>
2020-05-12 02:16:04 +0800

    wip (#8)
    * wip
    
    * wip

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+            return in_backend()
+                ? redirect()->route('backend.home')
+                : redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
