32c0499fe77839e61da941729f07a8462ffebf65
Jovert Palonpon <jovertical@gmail.com>
2020-05-24 20:05:20 +0800

    wip

--- deploy/composer.sh
+++ deploy/composer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $APP_ENV == "testing" && $APP_ENV == "production" ]]; then
+if [[ $APP_ENV == "production" ]]; then
     composer install -q --no-ansi --no-interaction --prefer-dist --optimize-autoloader
 fi
